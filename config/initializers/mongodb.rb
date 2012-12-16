197f71b8b91f58861908a63bbb5a799f74084788
alejandra <ale.estanislao@gmail.com>
2012-12-16 17:52:43 +0100

    Making things work in Heroku

--- config/initializers/mongodb.rb
+++ config/initializers/mongodb.rb
@@ -6,17 +6,3 @@
 #      MongoMapper.connection.connect if forked
 #    end
 # end
-
-require 'mongo'
-require 'uri'
-
-def get_connection
-  return @db_connection if @db_connection
-  db = URI.parse(ENV['MONGOHQ_URL'])
-  db_name = db.path.gsub(/^\//, '')
-  @db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
-  @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
-  @db_connection
-end
-
-db = get_connection
