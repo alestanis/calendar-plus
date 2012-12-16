e8fe8e44a14d2cc3f9583d0efa8546fc42cad79f
alejandra <ale.estanislao@gmail.com>
2012-12-16 17:21:31 +0100

    Making things work in Heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,21 +25,31 @@ class User
   
   # Omniauth-related
   def self.from_omniauth(auth)
+    puts "Inside from Omniauth"
+    puts auth.to_yaml
     user = where(auth.slice("provider", "uid")).first
+    puts "User id = #{user._id}" if user
     if user
       user.token = auth["credentials"]["token"]
+      puts "User Token = #{user.token}"
       user
     else
       create_from_omniauth(auth)
     end
+    puts "Leaving from Omniauth"
   end
 
   def self.create_from_omniauth(auth)
+    puts "Create from Omniauth"
     create! do |user|
+      puts "With Provider = #{auth["provider"]}"
       user.provider = auth["provider"]
+      puts "With Uid = #{auth["uid"]}"
       user.uid = auth["uid"] # Email for google
+      puts "With Name = #{auth["uid"].split('@')[0]}"
       user.name = auth["uid"].split('@')[0] # Login of gmail account, "user" in "user@gmail.com"
       # user.token = auth["credentials"]["token"]
+      puts "Leaving Create from Omniauth"
     end
   end
 end
