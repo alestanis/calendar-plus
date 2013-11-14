a8c37a312b9ef6920905e31378036edf70c046fc
alestanis <ale.estanislao@gmail.com>
2013-11-13 18:48:07 -0800

    Delete user.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < CalendarController #ApplicationController
   end
 
   def destroy
-    User.delete(user.id)
+    User.delete(session[:user_id])
     session[:user_id] = nil
     session[:token] = nil
     redirect_to root_url, notice: "Signed out!"
